3d447fda8a69db4ee013ce664516b750c943dd13
jackalcooper <jackalcooper@gmail.com>
2021-09-08 10:59:02 +0800

    refein

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
+/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
 /***/ 64774:
@@ -106822,4 +106822,3 @@ module.exports = require("zlib");
 /******/ 	
 /******/ })()
 ;
-//# sourceMappingURL=index.js.map
