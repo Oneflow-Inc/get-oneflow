b3a78751dbe272fb2ee4498e7c0c79d17cfd105d
jackalcooper <jackalcooper@gmail.com>
2021-09-13 16:10:06 +0800

    refien

--- src/cacheOutput.ts
+++ src/cacheOutput.ts
@@ -2,10 +2,7 @@ import * as core from '@actions/core'
 import * as cache from './utils/cache'
 async function run(): Promise<void> {
   try {
-    const key: string = core.getInput('key', {required: true})
-    const prefixes: string[] = core.getMultilineInput('prefixes', {
-      required: true
-    })
+    const keys: string[] = core.getMultilineInput('keys', {required: true})
     let runnerLabels: string[] = core.getMultilineInput('runner-labels', {
       required: true
     })
@@ -13,14 +10,13 @@ async function run(): Promise<void> {
       core.setFailed('runner-labels empty')
       return
     }
-    const keys = prefixes.map(x => x.concat(key))
     const found = await cache.lookupInKeys(keys)
     if (found) {
       runnerLabels = ['ubuntu-latest']
       core.setOutput('object', found)
     }
     core.setOutput('runs-on', runnerLabels)
-    core.setOutput('prefixes', prefixes)
+    core.setOutput('keys', keys)
     core.setOutput('cache-hit', !!found)
   } catch (error) {
     core.setFailed(error.message)
--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -11,17 +11,18 @@ function ciCacheBucketStore(): OSS {
   return store
 }
 
+const OBJECT_KEY = 'object.json'
 export async function saveKey(key: string, obj: unknown): Promise<void> {
   const store = ciCacheBucketStore()
   const buf = Buffer.from(JSON.stringify(obj, null, 2), 'utf8')
-  await store.put(key, buf, {timeout: 60 * 1000 * 60})
+  await store.put(key.concat(OBJECT_KEY), buf, {timeout: 60 * 1000 * 60})
 }
 
 export async function lookupInKeys(keys: string[]): Promise<unknown> {
   const store = ciCacheBucketStore()
   // TODO: support check keys have same values
   for await (const key of keys) {
-    const res = await store.get(key)
+    const res = await store.get(key.concat(OBJECT_KEY))
     return JSON.parse(res.content)
   }
   return null
