a1b4904a356e4618e110f7b1203e834a7cf38577
jackalcooper <jackalcooper@gmail.com>
2021-09-13 12:41:59 +0800

    refein

--- src/cacheOutput.ts
+++ src/cacheOutput.ts
@@ -4,13 +4,23 @@ async function run(): Promise<void> {
   try {
     // const keys: string[] = core.getMultilineInput('keys')
     let runnerLabels: string[] = core.getMultilineInput('runner-labels')
+    const prefixes: string[] = core.getMultilineInput('prefixes')
     // if (runnerLabels.includes('self-hosted') === false) {
     //   runnerLabels.concat('self-hosted')
     // }
-    if (runnerLabels === []) {
-      runnerLabels = runnerLabels.concat('ubuntu-latest')
+    const found = true
+    if (found) {
+      runnerLabels = ['ubuntu-latest']
+    } else {
+      if (runnerLabels.includes('self-hosted') === false) {
+        runnerLabels = runnerLabels.concat(['self-hosted'])
+      }
+      if (runnerLabels.includes('linux') === false) {
+        runnerLabels = runnerLabels.concat(['linux'])
+      }
     }
     core.setOutput('runs-on', runnerLabels)
+    core.setOutput('prefixes', prefixes)
   } catch (error) {
     core.setFailed(error.message)
   }
