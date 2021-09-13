fe7af81f3224402603ce41f5151f25a6c8a161f7
jackalcooper <jackalcooper@gmail.com>
2021-09-13 14:11:10 +0800

    refine

--- src/cacheOutput.ts
+++ src/cacheOutput.ts
@@ -4,6 +4,10 @@ async function run(): Promise<void> {
   try {
     // const keys: string[] = core.getMultilineInput('keys')
     let runnerLabels: string[] = core.getMultilineInput('runner-labels')
+    if (runnerLabels.length === 0) {
+      core.setFailed('runner-labels empty')
+      return
+    }
     const prefixes: string[] = core.getMultilineInput('prefixes')
     // if (runnerLabels.includes('self-hosted') === false) {
     //   runnerLabels.concat('self-hosted')
