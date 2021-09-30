acbfecff22efbf473046a43d5f396f54484c9458
jackalcooper <jackalcooper@gmail.com>
2021-09-30 11:55:37 +0800

    publishing

---
+++ dist/index.js/index.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";var e={};(()=>{e.r=e=>{if(typeof Symbol!=="undefined"&&Symbol.toStringTag){Object.defineProperty(e,Symbol.toStringTag,{value:"Module"})}Object.defineProperty(e,"__esModule",{value:true})}})();if(typeof e!=="undefined")e.ab=__dirname+"/";var r={};e.r(r);module.exports=r})();
---
+++ src/main.ts
@@ -0,0 +1,1 @@
+import * as gh from '@actions/github'
