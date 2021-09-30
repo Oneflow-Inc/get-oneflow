4300f947a5b3cf499e0796dfd3ffb916b6f6ff0e
jackalcooper <jackalcooper@gmail.com>
2021-09-30 11:57:25 +0800

    refein

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
 import * as gh from '@actions/github'
+import * as core from '@actions/core'
+import {runAndSetFailed} from './utils/util'
+
+runAndSetFailed(async () => {
+  core.info(JSON.stringify(gh.context, null, 2))
+})
