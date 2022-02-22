325f3cac66c93da0552f2aaa3ba2f082d218da36
tsai <jackalcooper@gmail.com>
2022-02-22 20:12:39 +0800

    publishing

--- src/utils/crashUnlessPriority.ts
+++ src/utils/crashUnlessPriority.ts
@@ -25,7 +25,7 @@ export async function checkPriorityPR(): Promise<void> {
     )
     if (pr.data.state === 'open') {
       throw new Error(
-        `This PR is not allowed to run as long as priority PR is still open :#${priorityPRNumber} `
+        `This PR is not allowed to run as long as priority PR is still open :#${pull_number} `
       )
     }
   }
