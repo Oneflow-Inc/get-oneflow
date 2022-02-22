b50005f87b1c8da4fee7d90edc87521923ba8dce
tsai <jackalcooper@gmail.com>
2022-02-22 20:06:01 +0800

    publishing

--- src/utils/crashUnlessPriority.ts
+++ src/utils/crashUnlessPriority.ts
@@ -1,5 +1,6 @@
 import {Octokit} from '@octokit/core'
 import * as core from '@actions/core'
+import * as gh from '@actions/github'
 
 export async function checkPriorityPR(): Promise<void> {
   const token = core.getInput('token')
@@ -10,16 +11,22 @@ export async function checkPriorityPR(): Promise<void> {
   const owner = 'Oneflow-Inc'
   const repo = 'oneflow'
   if (priorityPRNumber) {
+    const pull_number = parseInt(priorityPRNumber)
+    if (pull_number === gh.context.issue.number) {
+      return
+    }
     const pr = await octokit.request(
       'GET /repos/{owner}/{repo}/pulls/{pull_number}',
       {
         owner,
         repo,
-        pull_number: parseInt(priorityPRNumber)
+        pull_number
       }
     )
     if (pr.data.state === 'open') {
-      throw new Error(`Priority PR is still open :#${priorityPRNumber}`)
+      throw new Error(
+        `This PR is not allowed to run as long as priority PR is still open :#${priorityPRNumber} `
+      )
     }
   }
 }
