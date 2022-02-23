58686ee99df2c69a3bbeba1b1774295440ff7b25
tsai <jackalcooper@gmail.com>
2022-02-23 17:40:29 +0800

    publishing

--- src/utils/crashUnlessPriority.ts
+++ src/utils/crashUnlessPriority.ts
@@ -7,11 +7,21 @@ export async function checkPriorityPR(): Promise<void> {
   const octokit = new Octokit({auth: token})
   const owner = 'Oneflow-Inc'
   const repo = 'oneflow'
+  const priorityLabel = 'need-highest-priority'
   const priority_prs = (
     await octokit.request('GET /search/issues', {
       q: `label:need-highest-priority state:open repo:${owner}/${repo}`
     })
   ).data.items
+  const MAX_NUM = 1
+  if (priority_prs.length > MAX_NUM) {
+    core.warning(
+      `There are priority PRs: ${priority_prs.length} > ${MAX_NUM}.
+No priority is effective.
+Remove label '${priorityLabel}' from some PRs if you really need priority for your PR.`
+    )
+    return
+  }
   if (
     priority_prs.length > 0 &&
     !priority_prs.map(pr => pr.number).includes(gh.context.issue.number)
