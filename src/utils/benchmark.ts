07b01cd902fc4998e1cb0ad4d97abd2d8288cf94
tsai <jackalcooper@gmail.com>
2022-03-30 14:01:22 +0800

    publishing

--- src/utils/benchmark.ts
+++ src/utils/benchmark.ts
@@ -208,7 +208,6 @@ export async function benchmarkWithPytest(): Promise<void> {
         'no:cacheprovider',
         '--max-worker-restart=0',
         '-x',
-        '--durations=50',
         '--capture=sys'
       ].concat(args)
     )
