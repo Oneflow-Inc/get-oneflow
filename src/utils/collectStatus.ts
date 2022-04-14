c557881e8ae325f3ab19f5fb36c63024869a109c
tsai <jackalcooper@gmail.com>
2022-04-14 09:26:18 +0800

    publishing

--- src/utils/collectStatus.ts
+++ src/utils/collectStatus.ts
@@ -1,6 +1,7 @@
 import * as core from '@actions/core'
 import {Octokit} from '@octokit/core'
 import * as exec from '@actions/exec'
+import os from 'os'
 
 const token = core.getInput('token')
 const octokit = new Octokit({auth: token})
@@ -9,6 +10,9 @@ const repo = 'oneflow'
 
 export async function collectWorkflowRunStatus(): Promise<void> {
   const test_workflow_id = 'test.yml'
+  const oneflowSrc: string = core
+    .getInput('oneflow-src', {required: true})
+    .replace('~', os.homedir)
   const workflow_runs = await octokit.request(
     'GET /repos/{owner}/{repo}/actions/workflows/{workflow_id}/runs',
     {
@@ -21,6 +25,8 @@ export async function collectWorkflowRunStatus(): Promise<void> {
   )
   process.env['GITHUB_TOKEN'] = token
   workflow_runs.data.workflow_runs.map(wr => {
-    exec.exec('gh', ['run', 'view', `${wr.id}`, '--log-failed'])
+    exec.exec('gh', ['run', 'view', `${wr.id}`, '--log-failed'], {
+      cwd: oneflowSrc
+    })
   })
 }
