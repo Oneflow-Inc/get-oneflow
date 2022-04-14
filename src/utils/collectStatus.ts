0a18a52064068c01afbb3c8ad26e2bfab25c45cb
tsai <jackalcooper@gmail.com>
2022-04-14 09:17:28 +0800

    publishing

---
+++ src/utils/collectStatus.ts
@@ -0,0 +1,25 @@
+import * as core from '@actions/core'
+import {Octokit} from '@octokit/core'
+import * as exec from '@actions/exec'
+
+const token = core.getInput('token')
+const octokit = new Octokit({auth: token})
+const owner = 'Oneflow-Inc'
+const repo = 'oneflow'
+
+export async function collectWorkflowRunStatus(): Promise<void> {
+  const test_workflow_id = 'test.yml'
+  const workflow_runs = await octokit.request(
+    'GET /repos/{owner}/{repo}/actions/workflows/{workflow_id}/runs',
+    {
+      owner,
+      repo,
+      workflow_id: test_workflow_id,
+      per_page: 30,
+      status: 'failure'
+    }
+  )
+  workflow_runs.data.workflow_runs.map(wr => {
+    exec.exec('gh', ['run', 'view', `${wr.id}`, '--log-failed'])
+  })
+}
