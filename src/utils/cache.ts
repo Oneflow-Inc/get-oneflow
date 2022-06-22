b24bc22d58e3167d3eb22511afcb1e1f70a4919c
tsai <jackalcooper@gmail.com>
2022-06-22 10:31:57 +0800

    publishing

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -196,6 +196,7 @@ export function getPatterns(
     'oneflow/user/**/*.cu',
     'oneflow/ir/**/*.h',
     'oneflow/ir/**/*.cpp',
+    'oneflow/**/*.td',
     'oneflow/core/**/*.h',
     'oneflow/core/**/*.hpp',
     'oneflow/core/**/*.cpp',
