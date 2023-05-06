b34676c94707f8e4b438739ad811d65a68804434
tsai <jackalcooper@gmail.com>
2023-05-06 11:09:13 +0800

    publishing

--- src/utils/collectStatus.ts
+++ src/utils/collectStatus.ts
@@ -88,10 +88,8 @@ export async function collectWorkflowRunStatus(): Promise<void> {
             crlfDelay: Infinity
           })
           for await (const line of rl) {
-            if (line.includes('Error: CUDA out of memory')) {
-              core.warning(
-                `[Error: CUDA out of memory][${file}] ${wr.html_url}`
-              )
+            if (line.includes('CUDA out of memory')) {
+              core.warning(`[CUDA out of memory][${file}] ${wr.html_url}`)
             }
             const isFailure =
               line.includes('FAILURE') ||
