f1db69538e7343c66dffa0467124831500a96462
Shenghang Tsai <jackalcooper@gmail.com>
2024-03-19 11:38:57 +0800

    Whl skip nccl (#125)
    * check in ts
    
    * publishing
    
    * publishing

--- src/utils/docker.ts
+++ src/utils/docker.ts
@@ -190,7 +190,12 @@ async function buildAndMakeWheel(
       'libcublas.so.11',
       'libcublasLt.so.11',
       'libcublas.so.12',
-      'libcublasLt.so.12'
+      'libcublasLt.so.12',
+      'libnccl.so.2',
+      'libcufft.so.11',
+      'libcufft.so.12',
+      'libcusparse.so.11',
+      'libcusparse.so.12'
     ]
   }
   const nvLibsExcludes = Array.prototype.concat.apply(
